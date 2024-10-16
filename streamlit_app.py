23aac92accc2d098895f32c6432ec57fae3862cb
cde750 <cedo940@hotmail.com>
2024-10-16 19:54:24 +0000

    maj1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+ticker = 'SP5.PA'  # Code pour Amundi SP5 sur Yahoo Finance
+etf_data = yf.download(ticker, period="1y")
