02595e7e0e1d4dc3184e304938a851979e3d6fc5
cde750 <cedo940@hotmail.com>
2024-10-23 19:41:20 +0000

    modif2310

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,17 @@ import yfinance as yf
 import pandas as pd
 import plotly.graph_objs as go
 
+# Fonction mise en cache pour télécharger les données de yfinance
+@st.cache_data
+def fetch_data(ticker, period):
+    try:
+        # Télécharger les données depuis yfinance
+        data = yf.download(ticker, period=period)
+        return data
+    except Exception as e:
+        st.error(f"Erreur lors de la récupération des données pour {ticker} : {e}")
+        return None
+
 # Fonction pour charger la liste depuis un fichier
 def load_list(filename):
     try:
@@ -46,65 +57,62 @@ def display_candlestick(tickers, period, show_sma, sma_period, key_prefix):
 
         st.subheader(f"{title_prefix}Cours de {ticker} - {period} d'historique")
 
-        # Récupérer les données
-        try:
-            data = yf.download(ticker, period=period)
-            if data.empty:
-                st.warning(f"Aucune donnée trouvée pour {ticker}.")
-                continue  # Passer à l'ETF suivant si pas de données
+        # Récupérer les données en utilisant la fonction mise en cache
+        data = fetch_data(ticker, period)
+        
+        if data is None or data.empty:
+            st.warning(f"Aucune donnée trouvée pour {ticker}.")
+            continue
 
-            if not isinstance(data.index, pd.DatetimeIndex):
-                data.index = pd.to_datetime(data.index)
+        if not isinstance(data.index, pd.DatetimeIndex):
+            data.index = pd.to_datetime(data.index)
 
-            # Resample les données hebdomadaires
-            data = data.resample('W').agg({'Close': 'last', 'Open': 'first', 'High': 'max', 'Low': 'min'})
+        # Resample les données hebdomadaires
+        data = data.resample('W').agg({'Close': 'last', 'Open': 'first', 'High': 'max', 'Low': 'min'})
 
-            # Création du graphique en chandelier
-            fig = go.Figure(data=[go.Candlestick(
+        # Création du graphique en chandelier
+        fig = go.Figure(data=[go.Candlestick(
+            x=data.index,
+            open=data['Open'],
+            high=data['High'],
+            low=data['Low'],
+            close=data['Close'],
+            name=ticker
+        )])
+
+        # Ajouter la moyenne mobile simple si activée
+        if show_sma:
+            data['SMA'] = data['Close'].rolling(window=sma_period).mean()
+            fig.add_trace(go.Scatter(
                 x=data.index,
-                open=data['Open'],
-                high=data['High'],
-                low=data['Low'],
-                close=data['Close'],
-                name=ticker
-            )])
+                y=data['SMA'],
+                mode='lines',
+                name=f'SMA {sma_period} périodes',
+                line=dict(color='yellow', width=2)
+            ))
 
-            # Ajouter la moyenne mobile simple si activée
-            if show_sma:
-                data['SMA'] = data['Close'].rolling(window=sma_period).mean()
-                fig.add_trace(go.Scatter(
-                    x=data.index,
-                    y=data['SMA'],
-                    mode='lines',
-                    name=f'SMA {sma_period} périodes',
-                    line=dict(color='yellow', width=2)
-                ))
+        # Ajouter la ligne horizontale si une valeur est spécifiée pour ce ticker
+        if ticker in action_values:
+            fig.add_shape(type="line",
+                          x0=data.index.min(), x1=data.index.max(),
+                          y0=action_values[ticker], y1=action_values[ticker],
+                          line=dict(color="Red", width=2, dash="dash"),
+                          name=f'Valeur seuil {ticker}')
+            fig.add_trace(go.Scatter(
+                x=[data.index.min()],
+                y=[action_values[ticker]],
+                text=[f"Seuil: {action_values[ticker]}"],
+                mode="text",
+                showlegend=False
+            ))
 
-            # Ajouter la ligne horizontale si une valeur est spécifiée pour ce ticker
-            if ticker in action_values:
-                fig.add_shape(type="line",
-                              x0=data.index.min(), x1=data.index.max(),
-                              y0=action_values[ticker], y1=action_values[ticker],
-                              line=dict(color="Red", width=2, dash="dash"),
-                              name=f'Valeur seuil {ticker}')
-                fig.add_trace(go.Scatter(
-                    x=[data.index.min()],
-                    y=[action_values[ticker]],
-                    text=[f"Seuil: {action_values[ticker]}"],
-                    mode="text",
-                    showlegend=False
-                ))
+        fig.update_layout(
+            title=f"Cours de {ticker} - {period} d'historique",
+            xaxis_title='Date',
+            yaxis_title='Prix',
+        )
 
-            fig.update_layout(
-                title=f"Cours de {ticker} - {period} d'historique",
-                xaxis_title='Date',
-                yaxis_title='Prix',
-            )
-
-            st.plotly_chart(fig)
-        except Exception as e:
-            st.error(f"Erreur lors de la récupération des données pour {ticker} : {e}")
-
+        st.plotly_chart(fig)
 
 # Fonction pour afficher les courbes différentielles
 def display_differential_curves(tickers, ref_ticker, period, show_sma, sma_period, key_prefix):
@@ -114,46 +122,43 @@ def display_differential_curves(tickers, ref_ticker, period, show_sma, sma_perio
         
         st.subheader(f"Différentiel entre {ticker} et {ref_ticker}")
 
-        # Récupérer les données
-        try:
-            ref_data = yf.download(ref_ticker, period=period).resample('W').agg({'Close': 'last'})
-            ticker_data = yf.download(ticker, period=period).resample('W').agg({'Close': 'last'})
-            
-            if ref_data.empty or ticker_data.empty:
-                st.warning(f"Aucune donnée trouvée pour {ticker} ou {ref_ticker}.")
-                continue
+        # Récupérer les données en utilisant la fonction mise en cache
+        ref_data = fetch_data(ref_ticker, period)
+        ticker_data = fetch_data(ticker, period)
+        
+        if ref_data is None or ticker_data is None or ref_data.empty or ticker_data.empty:
+            st.warning(f"Aucune donnée trouvée pour {ticker} ou {ref_ticker}.")
+            continue
 
-            # Calcul du différentiel
-            diff_data = ticker_data['Close'] / ref_data['Close']
+        # Calcul du différentiel
+        diff_data = ticker_data['Close'] / ref_data['Close']
 
-            # Création du graphique différentiel
-            fig = go.Figure(data=[go.Scatter(
+        # Création du graphique différentiel
+        fig = go.Figure(data=[go.Scatter(
+            x=diff_data.index,
+            y=diff_data,
+            mode='lines',
+            name=f'Différentiel {ticker}/{ref_ticker}'
+        )])
+
+        # Ajouter la moyenne mobile simple si activée
+        if show_sma:
+            diff_data_sma = diff_data.rolling(window=sma_period).mean()
+            fig.add_trace(go.Scatter(
                 x=diff_data.index,
-                y=diff_data,
+                y=diff_data_sma,
                 mode='lines',
-                name=f'Différentiel {ticker}/{ref_ticker}'
-            )])
+                name=f'SMA {sma_period} périodes',
+                line=dict(color='yellow', width=2)
+            ))
 
-            # Ajouter la moyenne mobile simple si activée
-            if show_sma:
-                diff_data_sma = diff_data.rolling(window=sma_period).mean()
-                fig.add_trace(go.Scatter(
-                    x=diff_data.index,
-                    y=diff_data_sma,
-                    mode='lines',
-                    name=f'SMA {sma_period} périodes',
-                    line=dict(color='yellow', width=2)
-                ))
+        fig.update_layout(
+            title=f"Différentiel entre {ticker} et {ref_ticker}",
+            xaxis_title='Date',
+            yaxis_title='Ratio',
+        )
 
-            fig.update_layout(
-                title=f"Différentiel entre {ticker} et {ref_ticker}",
-                xaxis_title='Date',
-                yaxis_title='Ratio',
-            )
-
-            st.plotly_chart(fig)
-        except Exception as e:
-            st.error(f"Erreur lors de la récupération des données pour {ticker} ou {ref_ticker} : {e}")
+        st.plotly_chart(fig)
 
 # Onglets
 tab1, tab2, tab3, tab4, tab5 , tab6, tab7 = st.tabs(["Indices", "Indices - différentiels", "Actions", "Actions - différentiels", "Devises", "Recherche", "Recherche - différentiels"])
@@ -209,14 +214,14 @@ with tab2:
     if show_sma_diff:
         sma_diff_period = st.slider('Choisissez le nombre de périodes pour la SMA des courbes différentielles', min_value=5, max_value=100, value=30, key="sma_diff_period_etfs")
 
-    display_differential_curves(selected_etfs, etf_ref, period, show_sma_diff, sma_diff_period, key_prefix="etfs_diff")
+    display_differential_curves(selected_etfs, etf_ref, period, show_sma_diff, sma_diff_period, key_prefix="etf_diff")
 
 # Onglet 3 : Actions
 with tab3:
     st.subheader("Graphique en chandelier des Actions")
 
-    # Charger la liste des actions
-    selected_actions = load_list('action_list.txt')
+    # Charger la liste des Actions
+    selected_actions = load_list('actions_list.txt')
 
     selected_period = st.radio(
         "Choisissez la profondeur historique des données :",
@@ -226,13 +231,13 @@ with tab3:
     )
     period = "2y" if selected_period == '2 ans' else "5y"
 
-    # Saisie des actions
-    actions_input = st.text_input("Entrez les symboles des actions séparés par des virgules", ','.join(selected_actions), key="action_input")
+    # Saisie des Actions
+    actions_input = st.text_input("Entrez les symboles des Actions séparés par des virgules", ','.join(selected_actions), key="actions_input")
     actions = [action.strip() for action in actions_input.split(",")]
 
-    # Sauvegarder la liste des actions
-    if st.button("Sauvegarder la liste des actions"):
-        save_list('action_list.txt', actions)
+    # Sauvegarder la liste des Actions
+    if st.button("Sauvegarder la liste des Actions"):
+        save_list('actions_list.txt', actions)
 
     show_sma = st.checkbox('Afficher la moyenne mobile simple (SMA)', value=True, key="sma_actions")
     if show_sma:
@@ -244,22 +249,11 @@ with tab3:
 with tab4:
     st.subheader("Courbes différentielles entre les Actions")
 
-    # Charger la liste des actions
-    selected_actions = load_list('action_list.txt')
+    # Charger la liste des Actions
+    selected_actions = load_list('actions_list.txt')
 
-    # Ajouter un radio pour choisir la source de l'action de référence
-    ref_choice = st.radio(
-        "Choisissez la référence pour la division :", 
-        ('Entrer une action manuellement', '^FCHI', '^STOXX'), 
-        key="ref_choice"
-    )
-
-    # Si l'utilisateur choisit d'entrer une action manuellement, afficher une zone de texte
-    if ref_choice == 'Entrer une action manuellement':
-        action_ref = st.text_input('Entrez l\'action de référence pour la division', key="action_ref_diff")
-    else:
-        # Si l'utilisateur choisit ^FCHI ou ^STOXX, utiliser cette valeur
-        action_ref = ref_choice
+    # Choix de l'Action de référence
+    action_ref = st.selectbox('Choisissez l\'Action de référence pour la division', selected_actions, index=0, key="action_ref_diff")
 
     selected_period = st.radio(
         "Choisissez la profondeur historique des données :",
@@ -273,19 +267,13 @@ with tab4:
     if show_sma_diff:
         sma_diff_period = st.slider('Choisissez le nombre de périodes pour la SMA des courbes différentielles', min_value=5, max_value=100, value=30, key="sma_diff_period_actions")
 
-    # Affichage des courbes différentielles uniquement si une action de référence est sélectionnée ou entrée
-    if action_ref:
-        display_differential_curves(selected_actions, action_ref, period, show_sma_diff, sma_diff_period, key_prefix="actions_diff")
-    else:
-        st.warning("Veuillez entrer ou sélectionner une action de référence pour afficher les courbes différentielles.")
-
-
+    display_differential_curves(selected_actions, action_ref, period, show_sma_diff, sma_diff_period, key_prefix="action_diff")
 
 # Onglet 5 : Devises
 with tab5:
     st.subheader("Graphique en chandelier des Devises")
 
-    # Charger la liste des devises
+    # Charger la liste des Devises
     selected_devises = load_list('devises_list.txt')
 
     selected_period = st.radio(
@@ -296,12 +284,12 @@ with tab5:
     )
     period = "2y" if selected_period == '2 ans' else "5y"
 
-    # Saisie des devises
-    devises_input = st.text_input("Entrez les symboles des devises séparés par des virgules", ','.join(selected_devises), key="devise_input")
+    # Saisie des Devises
+    devises_input = st.text_input("Entrez les symboles des Devises séparés par des virgules", ','.join(selected_devises), key="devises_input")
     devises = [devise.strip() for devise in devises_input.split(",")]
 
-    # Sauvegarder la liste des devises
-    if st.button("Sauvegarder la liste des devises"):
+    # Sauvegarder la liste des Devises
+    if st.button("Sauvegarder la liste des Devises"):
         save_list('devises_list.txt', devises)
 
     show_sma = st.checkbox('Afficher la moyenne mobile simple (SMA)', value=True, key="sma_devises")
@@ -310,72 +298,55 @@ with tab5:
 
     display_candlestick(devises, period, show_sma, sma_period, key_prefix="devises")
 
-
-    # Onglet 6 : Recherche
+# Onglet 6 : Recherche
 with tab6:
-    st.subheader("Graphique en chandelier des valeurs recherchées")
+    st.subheader("Graphique en chandelier pour Recherche")
 
-    # Charger la liste des valeurs recherchées
-    selected_research = load_list('recherche.txt')
+    # Charger la liste des symboles
+    selected_recherche = load_list('recherche_list.txt')
 
     selected_period = st.radio(
         "Choisissez la profondeur historique des données :",
         ('2 ans', '5 ans'),
         index=1,
-        key="period_chandeliers_research"
+        key="period_chandeliers_recherche"
     )
     period = "2y" if selected_period == '2 ans' else "5y"
 
-    # Saisie des valeurs recherchées
-    research_input = st.text_input("Entrez les symboles des valeurs recherchées séparés par des virgules", ','.join(selected_research), key="research_input")
-    research_items = [item.strip() for item in research_input.split(",")]
+    # Saisie des symboles
+    recherche_input = st.text_input("Entrez les symboles séparés par des virgules", ','.join(selected_recherche), key="recherche_input")
+    recherche = [symb.strip() for symb in recherche_input.split(",")]
 
-    # Sauvegarder la liste des valeurs recherchées
-    if st.button("Sauvegarder la liste des valeurs recherchées"):
-        save_list('recherche.txt', research_items)
+    # Sauvegarder la liste des symboles
+    if st.button("Sauvegarder la liste des symboles pour Recherche"):
+        save_list('recherche_list.txt', recherche)
 
-    show_sma = st.checkbox('Afficher la moyenne mobile simple (SMA)', value=True, key="sma_research")
+    show_sma = st.checkbox('Afficher la moyenne mobile simple (SMA)', value=True, key="sma_recherche")
     if show_sma:
-        sma_period = st.slider('Choisissez le nombre de périodes pour la SMA', min_value=5, max_value=100, value=30, key="sma_period_research")
+        sma_period = st.slider('Choisissez le nombre de périodes pour la SMA', min_value=5, max_value=100, value=30, key="sma_period_recherche")
 
-    display_candlestick(research_items, period, show_sma, sma_period, key_prefix="research")
+    display_candlestick(recherche, period, show_sma, sma_period, key_prefix="recherche")
 
-
-# Onglet 7 : Recherche - Différentiels
+# Onglet 7 : Recherche - Courbes différentielles
 with tab7:
-    st.subheader("Courbes différentielles entre les éléments de Recherche")
+    st.subheader("Courbes différentielles pour Recherche")
 
-    # Charger la liste des éléments de recherche
-    selected_recherche = load_list('recherche.txt')
+    # Charger la liste des symboles
+    selected_recherche = load_list('recherche_list.txt')
 
-    # Ajouter un radio pour choisir la source de la référence
-    ref_choice_recherche = st.radio(
-        "Choisissez la référence pour la division :", 
-        ('Entrer une action ou indice manuellement', '^FCHI', '^STOXX', '^SPX'), 
-        key="ref_choice_recherche"
-    )
+    # Choix du symbole de référence
+    recherche_ref = st.selectbox('Choisissez le symbole de référence pour la division', selected_recherche, index=0, key="recherche_ref_diff")
 
-    # Si l'utilisateur choisit d'entrer manuellement, afficher une zone de texte
-    if ref_choice_recherche == 'Entrer une action ou indice manuellement':
-        recherche_ref = st.text_input('Entrez l\'action ou l\'indice de référence pour la division', key="recherche_ref_diff")
-    else:
-        # Si l'utilisateur choisit ^FCHI ou ^STOXX, utiliser cette valeur
-        recherche_ref = ref_choice_recherche
-
-    selected_period_recherche = st.radio(
+    selected_period = st.radio(
         "Choisissez la profondeur historique des données :",
         ('2 ans', '5 ans'),
         index=1,
         key="period_diff_recherche"
     )
-    period_recherche = "2y" if selected_period_recherche == '2 ans' else "5y"
+    period = "2y" if selected_period == '2 ans' else "5y"
 
-    show_sma_diff_recherche = st.checkbox('Afficher la moyenne mobile simple (SMA) pour les courbes différentielles', value=True, key="sma_diff_recherche")
-    if show_sma_diff_recherche:
-        sma_diff_period_recherche = st.slider('Choisissez le nombre de périodes pour la SMA des courbes différentielles', min_value=5, max_value=100, value=30, key="sma_diff_period_recherche")
+    show_sma_diff = st.checkbox('Afficher la moyenne mobile simple (SMA) pour les courbes différentielles', value=True, key="sma_diff_recherche")
+    if show_sma_diff:
+        sma_diff_period = st.slider('Choisissez le nombre de périodes pour la SMA des courbes différentielles', min_value=5, max_value=100, value=30, key="sma_diff_period_recherche")
 
-    # Affichage des courbes différentielles uniquement si une référence est sélectionnée ou entrée
-    if recherche_ref:
-        display_differential_curves(selected_recherche, recherche_ref, period_recherche, show_sma_diff_recherche, sma_diff_period_recherche, key_prefix="recherche_diff")
-    else:
-        st.warning("Veuillez entrer ou sélectionner une référence pour afficher les courbes différentielles.")
+    display_differential_curves(selected_recherche, recherche_ref, period, show_sma_diff, sma_diff_period, key_prefix="recherche_diff")
