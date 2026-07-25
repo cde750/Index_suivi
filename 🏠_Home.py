34c018d3aa223ce4f82d934fd49df7ef04e8154a
cde750 <cedo940@hotmail.com>
2026-07-25 08:52:49 +0200

    Add files via upload

---
+++ 🏠_Home.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import streamlit as st
+
+st.set_page_config(page_title="PIF Prévis", page_icon="🛫", layout="centered", initial_sidebar_state="auto")
+
+
+st.title('🛫 PIF Prévis') 
+st.divider()
+
+with st.sidebar.expander("Version"):
+    st.sidebar.info("")
+    
+
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
