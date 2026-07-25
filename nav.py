f60ceb68abbb2fba5bd42ea83fe8a7763e5c8762
cde750 <cedo940@hotmail.com>
2026-07-25 21:33:01 +0200

    Update nav.py

--- nav.py
+++ nav.py
@@ -2,8 +2,8 @@
 import streamlit as st
 
 page_a = st.Page("streamlit_app.py", title="Accueil", icon="🏠")
-page_b = st.Page("momentum.py", title="SP 500", icon="📊")
-page_c = st.Page("momentum_EU.py", title="Eurostoxx 50", icon="📊")
+page_b = st.Page("momentum.py", title="SP 500", icon="🇺🇸")
+page_c = st.Page("momentum_EU.py", title="Eurostoxx 50", icon="🇪🇺")
 
 pg = st.navigation([page_a, page_b, page_c])
 pg.run()
