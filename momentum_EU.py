4e23616214cac413ef3603bd7965cd642adf5710
cde750 <cedo940@hotmail.com>
2026-07-25 19:01:01 +0200

    Update momentum_EU.py

--- momentum_EU.py
+++ momentum_EU.py
@@ -86,11 +86,7 @@ def get_sp500_tickers():
     # Dictionnaire ticker -> secteur
     sectors = dict(zip(table["Ticker"], table["Sector"]))
 
-     # --- DEBUG (à retirer) ---
-    st.write("DEBUG - tableau complet :")
-    st.dataframe(table)
-    st.write("DEBUG - tickers :", tickers)
-    # --- fin DEBUG ---
+    
     
     return tickers, sectors
 
