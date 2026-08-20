f37a9ba2cd576e9c8b03a602bf2721565a8d5eec
cde750 <cedo940@hotmail.com>
2026-08-20 22:43:02 +0200

    Update nav.py

--- nav.py
+++ nav.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 page_a = st.Page("streamlit_app.py", title="Accueil", icon="🏠")
-page_b = st.Page("momentum2.py", title="SP 500", icon="🇺🇸")
+page_b = st.Page("momentum.py", title="SP 500", icon="🇺🇸")
 page_c = st.Page("momentum_EU.py", title="Eurostoxx 50", icon="🇪🇺")
 page_d = st.Page("Ey.py", title="Ey")
 
